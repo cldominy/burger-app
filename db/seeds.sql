16aa65dcd17afce0a873487a3a84de975cbbd946
Chris Dominy <cdominy@utexas.edu>
2020-10-18 14:28:13 -0500

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES 
+("Big Mac"),
+("Whooper"),
+("Whataburger");
+
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("Baconator", true),
+("Whooper Junior", true);
